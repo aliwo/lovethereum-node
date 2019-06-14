c93bdee5b8dec27f7b9722e4b28cf8313b2a0c5d
aliwo <aliwo@naver.com>
2019-06-14 13:06:28 +0900

    lint 수정

--- src/Models/Love.js
+++ src/Models/Love.js
@@ -1,10 +1,10 @@
 import mongoose, { mongo } from 'mongoose';
 
-cosnt LoveSchema = mongoose.Schema({
-    url : String,
-    message : String
+const LoveSchema = mongoose.Schema({
+    url: String,
+    message: String,
 });
 
-const model = mongoose.Model('Love',LoveSchema);
+const model = mongoose.Model('Love', LoveSchema);
 
 export default model;
