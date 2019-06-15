9dd1deb4f818d27f958dff88384e23d27528b7dc
aliwo <aliwo@naver.com>
2019-06-15 22:30:16 +0900

    아까 빠진 커밋...

---
+++ test/testGenUrl.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { randomUrl } = require('../src/Libs/RandomUrl');
+
+(function () {
+    suite('generate url', () => {
+        test('generate url', () => {
+            assert(randomUrl().length > 4);
+        });
+    });
+}).call(this);
