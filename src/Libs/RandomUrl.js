19e9dc7ac9c7fdd9a053cd27acc26b1f321fc1fb
aliwo <aliwo@naver.com>
2019-06-16 19:47:48 +0900

    찐또뺴기 random url 을 뱉도록 수정

--- src/Libs/RandomUrl.js
+++ src/Libs/RandomUrl.js
@@ -1,10 +1,10 @@
+let sha1 = require('js-sha1');
 
 export const randomUrl = () => {
-    let firstPart = (Math.random() * 46656) | 0;
+    let hash = sha1(new Date().toISOString());
     let secondPart = (Math.random() * 46656) | 0;
-    firstPart = `000${firstPart.toString(36)}`.slice(-3);
     secondPart = `000${secondPart.toString(36)}`.slice(-3);
-    return firstPart + secondPart;
+    return hash.slice(0, 10) + secondPart;
 };
 
 export const nothing = () => 3;
